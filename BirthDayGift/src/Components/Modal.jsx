2734a349217965eba0fbb26e204a6733c8f07eff
Pixzzz <122415616+Pixzzz@users.noreply.github.com>
2025-07-29 16:04:34 -0400

    cambios a la rama

--- BirthDayGift/src/Components/Modal.jsx
+++ BirthDayGift/src/Components/Modal.jsx
@@ -15,7 +15,7 @@ export const Modal = ({ isOpen, OnClose }) => {
             Hola ser enano que rapté para que sea mi be fren, 1ro hapi verdi,
             espero que sigas cumpliendo muchos años de tu bella vida, agradecido
             toy' de tener como mi be fren y de las personas mas especiles que
-            tengo, y que confio tamb lol, a pesar de la mrd que soy, sigue
+            tengo, y que confio tamb lol, a pesar de la mrd que soy. Sigue
             viviendo y cumpliendo ma' año de vida, si? Gracias. TE
             QUIEROOOOOOOOOOOOOOOOOOOOOOOOOOOO <br />
             <span className=" text-4xl">♡</span> <br /> (cuando cobre
