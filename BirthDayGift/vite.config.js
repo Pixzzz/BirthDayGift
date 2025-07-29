2f2092ee4e968299b4051e5a798e76e4910c2bfe
Pixzzz <122415616+Pixzzz@users.noreply.github.com>
2025-07-29 19:46:39 -0400

    .

--- BirthDayGift/vite.config.js
+++ BirthDayGift/vite.config.js
@@ -5,8 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: "dist",
-  },
   base: "./",
+  build: {
+    outDir: 'dist',
+  },
 });
